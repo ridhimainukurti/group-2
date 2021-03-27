cf377158c080f23e919a4259d6d39c09d083c4d6
sriyachilla <73271452+sriyachilla@users.noreply.github.com>
2021-03-26 23:01:00 -0700

    image of map added

--- view.py
+++ view.py
@@ -1,4 +1,16 @@
+# https://flask.palletsprojects.com/en/1.1.x/api/
+from flask import Flask, render_template
+#create a Flask instance
+app = Flask(__name__, template_folder='templates')
 
-
+#connects default URL of server to a python function
+@app.route('/')
 @app.route('/home')
-def home_route()
+def home():
+    #function use Flask import (Jinja) to render an HTML template
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    #runs the application on the repl development server
+    app.run(debug=True)
+    #app.run(debug=True, port="5001", host="127.0.0.1")
