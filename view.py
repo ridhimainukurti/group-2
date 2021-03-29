deda4f3805293020ae991d332a839ca0bdaf73f6
ridhimainukurti <45546576+ridhimainukurti@users.noreply.github.com>
2021-03-28 18:02:17 -0700

    updated blueprints

--- view.py
+++ view.py
@@ -1,7 +1,7 @@
 # https://flask.palletsprojects.com/en/1.1.x/api/
 from flask import Flask, render_template
 from Blueprints.events.app import addevent_bp, calendar_bp
-from Blueprints.minilab.app import ridhima_bp, sriya_bp
+from Blueprints.minilab.app import ridhima_bp, sriya_bp, isai_bp
 
 #create a Flask instance
 app = Flask(__name__)
@@ -9,7 +9,7 @@ app.register_blueprint(addevent_bp, url_prefix='/addevent')
 app.register_blueprint(calendar_bp, url_prefix='/calendar')
 app.register_blueprint(ridhima_bp, url_prefix='/ridhima')
 app.register_blueprint(sriya_bp, url_prefix='/sriya')
-app.register_blueprint(sriya_bp, url_prefix='/isai')
+app.register_blueprint(isai_bp, url_prefix='/isai')
 
 
 #connects default URL of server to a python function
